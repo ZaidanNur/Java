cb45718c7af6dc963ea67262dfd688c40e717fb4
ZaidanNur <zaixyz332@gmail.com>
2021-09-06 15:57:28 +0700

    change on number_withno...

--- Tugas1/src/com/tugasku/print100withnonumber/number_withno_number.java
+++ Tugas1/src/com/tugasku/print100withnonumber/number_withno_number.java
@@ -2,9 +2,8 @@ package com.tugasku.print100withnonumber;
 
 public class number_withno_number {
     public static void main(String[] args){
-        String one = "A";
         String ten = "AAAAAAAAAA";
-        for(int count =one.length();count<=(ten.length()*ten.length());count++){
+        for(int count =(ten.length()*ten.length());count>0;count--){
             System.out.println(count);
         }
     }
